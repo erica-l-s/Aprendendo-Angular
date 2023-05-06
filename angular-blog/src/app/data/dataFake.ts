40286c508e6f304d97756e7533e4b24f0d518779
erica-l-s <erica.l.s1996@gmail.com>
2023-05-06 12:48:00 -0300

    criando novas paginas

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo":"aaaaa",
+        "title":"titulo é titulo",
+        "description":"aki vem a descriçao"
+    },
+    {
+        "id":2,
+        "photo":"aaaaa",
+        "title":"titulo é titulo",
+        "description":"aki vem a descriçao"
+    },
+]
